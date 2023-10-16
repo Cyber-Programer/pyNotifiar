5d950690ad6d5d6f66be0524ad2ba0fb3818c61d
MD siFaT isLaM <125746506+Cyber-Programer@users.noreply.github.com>
2023-10-16 18:30:37 +0600

    Add files via upload

---
+++ notify.py
@@ -0,0 +1,12 @@
+from plyer import notification
+         
+data = 'Thanks for vesiting my github page'
+
+def notify():
+    notification.notify(
+        title='Cyber-Programer',
+        message= data,
+
+    )
+
+notify()
